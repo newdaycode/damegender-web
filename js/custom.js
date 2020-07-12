b75de722184be9548c7f148d3fed2ec2cbf1d3b4
David Arroyo Menéndez <d.arroyome@alumnos.urjc.es>
2020-07-12 13:12:20 +0200

    js/custom.js: updates

--- js/custom.js
+++ js/custom.js
@@ -1,12 +1,16 @@
+
 var description = [
-    "img/logos/Ada_Lovelace.jpg",
+    "img/logos/1024px-Caravaggio_-_Medusa_-_Google_Art_Project.jpg",
+    "img/logos/800px-Bartolomé_Esteban_Perez_Murillo_014.jpg",
+    "img/logos/800px-Igualtat_de_sexes.svg.png",
+    "img/logos/Ada_Lovelace.jpg",    
     "img/logos/Grace_Murray_Hopper.jpg",
     "img/logos/adaneva.jpg",
-    "img/logos/menendez.jpg",
-    "img/logos/xfiles.png",
-    "img/logos/th2.png",
-    "img/logos/Medusa_by_Caravaggio.jpg"            
+    "img/logos/illuminati.png",
+    "img/logos/stolen_kiss.jpg",
+    "img/logos/xfiles.png"    
 ];
+
 var size = description.length
 var x = Math.floor(size*Math.random())
 document.getElementById('logo').src=description[x];
